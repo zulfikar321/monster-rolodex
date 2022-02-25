01388188ccb6e07fa2071403bc0df85d0692fd19
Zulfikar Muhamad <zulfikarm832@gmail.com>
2022-02-25 13:55:31 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -32,7 +32,7 @@ class App extends Component {
     );
     return (
       <div className="App">
-        <h1>Monster Rolodex</h1>
+        <h1>Hello</h1>
         <SearchBox
           placeholder="search monster"
           handlechange={this.handlechange}
